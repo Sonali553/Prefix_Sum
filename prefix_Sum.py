ac67a6e4007202cd913ddb99249ebef09d0e0439
Sonali553 <83964942+Sonali553@users.noreply.github.com>
2023-07-14 12:46:05 +0530

    Update prefix_Sum.py

--- prefix_Sum.py
+++ prefix_Sum.py
@@ -1,14 +1,11 @@
 #Prefix Sum
 lst = list(map(int, input().split()))
-prefix_sum = lst
 def prefix_Sum(lst):
-    prefix_sum = lst
-    prefix_sum[0] = lst[0]
     for i in range(1, len(lst)):
-        prefix_sum[i] = prefix_sum[i - 1] + lst[i]
-    return prefix_sum
+        lst[i] = lst[i - 1] + lst[i]
+    return lst
 print(prefix_Sum([3, 6, 9, 2, 7]))
 print(prefix_Sum([1, 2, 3, 4, 5]))
-print(prefix_Sum([-1, -2, -3, 6, 9]))
 print(prefix_Sum([4, 3, 2]))
+print(prefix_Sum([-1, -2, -3, 6, 9]))
 print(prefix_Sum(lst))
